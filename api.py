4b3280039c4a77b8e3ecd6eb5214eed43d5d0af1
Johannes Hötter <johannes.hoetter@kern.ai>
2022-10-21 21:35:13 +0200

    add index message

--- api.py
+++ api.py
@@ -4,8 +4,21 @@ from langdetect import detect
 api = FastAPI()
 
 
+@api.get("/")
+async def root():
+    return {"message": "Hello World"}
+
 @api.post("/language_detection/")
 async def language_detection(request: Request):
+    """Detect language of text
+    
+    Args:
+        request (Request): Request object
+
+    Returns:
+        dict: Language of text
+    """
+
     body = await request.json()
     text = body["text"]
     language = detect(text)
