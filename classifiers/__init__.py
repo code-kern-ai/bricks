3d91771651230ed5862a3fbb83fdd5b191604027
Johannes Hötter <johannes.hoetter@kern.ai>
2022-10-21 22:27:36 +0200

    adds text complexity check

--- classifiers/__init__.py
+++ classifiers/__init__.py
@@ -1,16 +1,13 @@
 from fastapi import APIRouter, Request
 from .language_detection import fn_language_detection
+from .sentence_complexity import fn_sentence_complexity
 
 router = APIRouter()
 
 @router.post('/language_detection')
 async def language_detection(request: Request):
-    """Detect language of text
-
-    Args:
-        request (Request): request body
-
-    Returns:
-        dict: Language of text
-    """
     return fn_language_detection(await request.json())
+
+@router.post('/sentence_complexity')
+async def sentence_complexity(request: Request):
+    return fn_sentence_complexity(await request.json())
