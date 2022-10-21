0f27ec29af4abf580b1777933455dcf1ffb97787
Johannes Hötter <johannes.hoetter@kern.ai>
2022-10-21 22:00:47 +0200

    update index message

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 import classifiers
 
@@ -6,6 +7,17 @@ api = FastAPI()
 
 @api.get("/")
 async def root():
-    return {"message": "Hello World"}
+    html_content = """
+    <html>
+        <head>
+            <title>Kern AI - Content Library Endpoints</title>
+        </head>
+        <body>
+            <h1>Endpoints for the <a href="https://kern.ai" target="_blank">Kern AI</a> Content Library</h1>
+            <p>Please look into the content library to see how to use these endpoints.</p>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200) 
 
 api.include_router(classifiers.router, prefix='/classifiers')
