a9fedadce9468af1abd67108dcb5f9e55149bb91
Divyanshu Katiyar <divyanshukatiyar@Divyanshus-MBP.fritz.box>
2022-10-27 18:06:59 +0200

    implement the date extraction, with regex EXCLUDING the month of August

--- extractors/__init__.py
+++ extractors/__init__.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from .python_functions.url_extraction import fn_url_extraction, UrlExtractionModel
 from .python_functions.hashtag_extraction import hash_ext, HashExtraction
 from .python_functions.email_extraction import email_ext, EmailExtraction
+from .python_functions.date_extraction import date_ext, DateExtraction
 
 router = APIRouter()
 
@@ -16,3 +17,7 @@ def hash_extraction(request: HashExtraction):
 @router.post('/email_extraction')
 def email_extraction(request: EmailExtraction):
     return email_ext(request)
+
+@router.post('/date_extraction')
+def date_extraction(request: DateExtraction):
+    return date_ext(request)
---
+++ extractors/python_functions/date_extraction.py
@@ -0,0 +1,24 @@
+from typing import Optional, Union
+from pydantic import BaseModel
+from extractors.util.spacy import SpacySingleton
+import re
+
+
+class DateExtraction(BaseModel):
+    text: str
+    spacy_tokenizer: Optional[str] = "en_core_web_sm"
+
+def date_ext(request: DateExtraction):
+    text = request.text
+    nlp = SpacySingleton.get_nlp(request.spacy_tokenizer)
+    doc = nlp(text)
+    regex = re.compile(r"[(0-9|Jan(uary)|Feb(ruary)|Mar(ch)|Apr(il)|May|Jun(e)|Jul(y)|Sep(tember)|Oct(ober)|Nov(ember)|Dec(ember))\s]+[\w\./-][0-9A-Za-z]+[./,-][\s0-9]+")
+    regex.findall(text)
+
+    spans = []
+    for match in regex.finditer(text):
+        start, end = match.span()
+        span = doc.char_span(start, end)
+        spans.append([span.start, span.end, span.text])
+
+    return {"spans": spans}
