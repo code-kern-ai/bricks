17a2509a704db1bde205c1f5c3a9f5990dfc16df
Divyanshu Katiyar <divyanshukatiyar@Divyanshus-MBP.fritz.box>
2022-10-31 15:47:22 +0100

    implement the name extraction using spacy

---
+++ extractors/python_functions/name_extraction.py
@@ -0,0 +1,28 @@
+from typing import Optional
+from pydantic import BaseModel
+from extractors.util.spacy import SpacySingleton
+
+class NameExtractionModel(BaseModel):
+    text: str
+    spacy_tokenizer: str[Optional] = "en_core_web_lg"
+
+def name_extraction(request: NameExtractionModel):
+
+    """
+    Returns a dictionary of the extracted names from a given text.
+    """
+
+    text = request.text
+    nlp = SpacySingleton.get_nlp(request.spacy_tokenizer)
+    doc = nlp(text)
+    name = []
+
+    for entity in doc.ents:
+        if entity.label_ == 'PERSON':
+            name.append(entity)
+    # "name" will contain all the occurrences of a particular name.
+    # This is because spacy treats each word in a text as a unique vector.
+    # So, two occurrences of "Div" does not mean "Div" == "Div"!
+    names = {"Extracted names": names}
+
+    return names
