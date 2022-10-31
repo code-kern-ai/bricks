dd38bf268798c01b8d5d03c146f367a5a3aa1c3f
Leonard Püttmann <72195901+LeonardPuettmann@users.noreply.github.com>
2022-10-31 14:32:06 +0100

    Added reading time, syllable count and levenshtein distance (#5)
    * Added reading time, syllable count and levenshtein distance
    
    * Changed the naming in the levenshtein function and added the needed python package to the requirements.txt.
    
    * Fixed typo

--- classifiers/__init__.py
+++ classifiers/__init__.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
-from .python_functions.language_detection import fn_language_detection, LanguageDetectionModel
+from .python_functions.language_detection import LanguageDetectionModel, fn_language_detection
 from .python_functions.sentence_complexity import SentenceComplexityModel, fn_sentence_complexity
+from .python_functions.reading_time import ReadingTimeModel, fn_reading_time
+from .python_functions.syllable_count import SyllableCountModel, fn_syllable_count
+from .python_functions.levenshtein_distance import LevenshteinDistanceModel, fn_levenshtein_distance
 
 router = APIRouter()
 
@@ -11,3 +14,15 @@ def language_detection(request: LanguageDetectionModel):
 @router.post('/sentence_complexity')
 def sentence_complexity(request: SentenceComplexityModel):
     return fn_sentence_complexity(request)
+
+@router.post('/reading_time')
+def reading_time(request: ReadingTimeModel):
+    return fn_reading_time(request)
+
+@router.post('/syllable_count')
+def syllable_count(request: SyllableCountModel):
+    return fn_syllable_count(request)
+
+@router.post('/levenshtein_distance')
+def levenshtein_distance(request: SyllableCountModel):
+    return fn_levenshtein_distance(request)
