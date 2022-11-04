40bda40bae4cae2c8e423701daed3a7f51592844
Divyanshu Katiyar <divyanshukatiyar@Divyanshus-MBP.fritz.box>
2022-11-04 11:41:41 +0100

    changed the keys of the final dict to lowercase and added the endpoint in __init__.py

--- extractors/__init__.py
+++ extractors/__init__.py
@@ -3,6 +3,7 @@ from .python_functions.url_extraction import fn_url_extraction, UrlExtractionMod
 from .python_functions.hashtag_extraction import hash_ext, HashExtraction
 from .python_functions.email_extraction import email_ext, EmailExtraction
 from .python_functions.date_extraction import date_ext, DateExtraction
+from .python_functions.price_extraction import price_extractor, PriceExtractionModel
 
 router = APIRouter()
 
@@ -21,3 +22,7 @@ def email_extraction(request: EmailExtraction):
 @router.post('/date_extraction')
 def date_extraction(request: DateExtraction):
     return date_ext(request)
+
+@router.post('/price_extraction')
+def price_extraction(request: PriceExtractionModel):
+    return price_extractor(request)
--- extractors/python_functions/price_extraction.py
+++ extractors/python_functions/price_extraction.py
@@ -7,7 +7,15 @@ class PriceExtractionModel(BaseModel):
     text: str
     spacy_tokenizer: Optional[str] = "en_core_web_lg"
 
-def price_ext(request: PriceExtractionModel):
+    class Config:
+        schema = {
+            "example": {
+                "text": "A desktop with i7 processor costs 800 euros in Germany. In the US, it costs 950 dollars.",
+                "spacy_tokenizer": "en_core_web_lg"
+            }
+        }
+
+def price_extractor(request: PriceExtractionModel):
     text = request.text
     nlp = SpacySingleton.get_nlp(request.spacy_tokenizer)
     doc = nlp(text)
@@ -17,4 +25,5 @@ def price_ext(request: PriceExtractionModel):
         if entity.label_ == 'MONEY':
             prices.append((entity.start, entity.end, entity))
 
-    return {"Prices": prices}
+    return {"extracted prices": prices}
+
