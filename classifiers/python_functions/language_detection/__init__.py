edc01eb4d02282c9012b30a11d4d9ace1166e89a
Johannes Hötter <johannes.hoetter@kern.ai>
2022-11-06 17:27:32 +0100

    source code in markdown

--- classifiers/python_functions/language_detection/__init__.py
+++ classifiers/python_functions/language_detection/__init__.py
@@ -27,20 +27,3 @@ def fn_language_detection(request: LanguageDetectionModel):
     language = detect(text)
     return {"language": language}
 
-### Code for this in Strapi would look as follows:
-# from typing import Dict, Any
-# from langdetect import detect
-
-# def fn_language_detection(record: Dict[str, Any]) -> str:
-#     """Detect language of text
-        
-#     Args:
-#         record (Dict): one single record you want to process
-
-#     Returns:
-#         str: Language of your text
-#     """
-
-#     text = record["your-text"]
-#     language = detect(text)
-#     return language
