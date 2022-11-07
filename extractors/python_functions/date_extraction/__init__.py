dace795882b96f60dff2afa8e1e65fa111d8072a
Johannes Hötter <johannes.hoetter@kern.ai>
2022-11-07 14:12:24 +0100

    adds email and date endpoints as configurable modules

--- extractors/python_functions/date_extraction/__init__.py
+++ extractors/python_functions/date_extraction/__init__.py
@@ -3,24 +3,29 @@ from pydantic import BaseModel
 from extractors.util.spacy import SpacySingleton
 import re
 
+INPUT_EXAMPLE = {
+    "text": "Today is 04.11.2022. Yesterday was 03/11/2022. Tomorrow is 05-11-2022. Day after tomorrow is 6 Nov 2022.",
+    "spacyTokenizer": "en_core_web_sm",
+}
+
 
 class DateExtractionModel(BaseModel):
     text: str
     spacyTokenizer: Optional[str] = "en_core_web_sm"
 
     class Config:
-        schema_extra = {
-            "example": {
-                "text": "Today is 04.11.2022. Yesterday was 03/11/2022. Tomorrow is 05-11-2022. Day after tomorrow is 6 Nov 2022.",
-                "spacyTokenizer": "en_core_web_sm",
-            }
-        }
+        schema_extra = {"example": INPUT_EXAMPLE}
 
-def date_extractor(request: DateExtractionModel):
+
+def date_extraction(request: DateExtractionModel):
+    """Detects dates in a text and returns them in a list."""
+
     text = request.text
     nlp = SpacySingleton.get_nlp(request.spacyTokenizer)
     doc = nlp(text)
-    regex = re.compile(r"(?:[0-9]{1,2}|Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)[\/\. -]{1}(?:[0-9]{1,2}|Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)[,\/\. -]{1}(?:[0-9]{2,4})")
+    regex = re.compile(
+        r"(?:[0-9]{1,2}|Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)[\/\. -]{1}(?:[0-9]{1,2}|Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)[,\/\. -]{1}(?:[0-9]{2,4})"
+    )
 
     dates = []
     for match in regex.finditer(text):
--- extractors/python_functions/email_extraction/__init__.py
+++ extractors/python_functions/email_extraction/__init__.py
@@ -3,19 +3,22 @@ from pydantic import BaseModel
 from extractors.util.spacy import SpacySingleton
 import re
 
+INPUT_EXAMPLE = {
+    "text": "If you have any questions, please contact johannes.hoetter@kern.ai.",
+    "spacyTokenizer": "en_core_web_sm",
+}
+
+
 class EmailExtractionModel(BaseModel):
     text: str
     spacyTokenizer: Optional[str] = "en_core_web_sm"
 
     class Config:
-        schema_extra = {
-            "example": {
-                "text": "If you have any questions, please contact johannes.hoetter@kern.ai.",
-                "spacyTokenizer": "en_core_web_sm"
-            }
-        }
+        schema_extra = {"example": INPUT_EXAMPLE}
 
-def email_extractor(request: EmailExtractionModel):
+
+def email_extraction(request: EmailExtractionModel):
+    """Detects emails in a text and returns them in a list."""
     text = request.text
     nlp = SpacySingleton.get_nlp(request.spacyTokenizer)
     doc = nlp(text)
