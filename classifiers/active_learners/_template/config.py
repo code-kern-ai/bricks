21fa314b8a7ffd0e037009c3063044b4dc040a3d
Johannes Hötter <johannes.hoetter@kern.ai>
2022-11-09 14:06:29 +0100

    fix input_example bug in configs

--- classifiers/active_learners/_template/config.py
+++ classifiers/active_learners/_template/config.py
@@ -1,12 +1,11 @@
 from util.configs import build_classifier_learner_config
 from util.enums import State
-from . import my_clf, INPUT_EXAMPLE
+from . import my_clf
 
 
 def get_config():
     return build_classifier_learner_config(
         function=my_clf,
-        input_example=INPUT_EXAMPLE,
         data_type="text",
         issue_id=0,  # you need to look this up in the issues https://github.com/code-kern-ai/bricks/issues
         state=State.DRAFT,  # make this State.PUBLIC when you are ready to publish
