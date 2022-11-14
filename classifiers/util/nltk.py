f9afb82db006ae1e9c7202d01e6dc8e4b0d69fea
Divyanshu Katiyar <divyanshukatiyar@Divyanshus-MBP.fritz.box>
2022-11-14 14:27:29 +0100

    Add ssl certificate verification for nltk download

--- classifiers/util/nltk.py
+++ classifiers/util/nltk.py
@@ -1,4 +1,12 @@
 import nltk
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
 
 
 def download_corpora():
