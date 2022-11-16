38e3e24d2c5844afe4bc856f0971f526becb1e00
Leonard Püttmann <leopuettmann@gmail.com>
2022-11-16 13:55:52 +0100

    Added builder for the generators

--- util/configs.py
+++ util/configs.py
@@ -76,6 +76,40 @@ def build_extractor_learner_config(
         state=state,
     )
 
+def build_generator_function_config(
+    function: Callable,
+    input_example: Dict[str, Any],
+    data_type: str,
+    issue_id: int,
+    state: str,
+):
+    return build_config(
+        module_type="generator",
+        execution_type="pythonFunction",
+        function=function,
+        input_example=input_example,
+        data_type=data_type,
+        issue_id=issue_id,
+        state=state,
+    )
+
+
+def build_generator_learner_config(
+    function: Callable,
+    data_type: str,
+    issue_id: int,
+    state: str,
+):
+    return build_config(
+        module_type="generator",
+        execution_type="activeLearner",
+        function=function,
+        input_example={"dummy": None},
+        data_type=data_type,
+        issue_id=issue_id,
+        state=state,
+    )
+
 
 def build_config(
     module_type: str,
