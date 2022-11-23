f2a12aa4f982da6306c76e91b5846436a37f8429
Leonard Püttmann <leopuettmann@gmail.com>
2022-11-23 13:46:31 +0100

    Added digit extraction

---
+++ extractors/python_functions/digit_extraction/config.py
@@ -0,0 +1,13 @@
+from util.configs import build_extractor_function_config
+from util.enums import State
+from . import digit_extraction, INPUT_EXAMPLE
+
+
+def get_config():
+    return build_extractor_function_config(
+        function=digit_extraction,
+        input_example=INPUT_EXAMPLE,
+        data_type="text",
+        issue_id=46, 
+        state=State.PUBLIC,
+    )
