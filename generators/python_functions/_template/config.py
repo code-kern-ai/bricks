4952a766a3461c7cb2a4c7f6e632c8d7f3336a21
Leonard Püttmann <leopuettmann@gmail.com>
2022-12-01 15:20:28 +0100

    Added missing input example to template config

--- generators/python_functions/_template/config.py
+++ generators/python_functions/_template/config.py
@@ -6,7 +6,7 @@ from . import _template, INPUT_EXAMPLE
 def get_config():
     return build_generator_function_config(
         function=_template,
-        input_example=INPUT_EXAMPLE,
+        input_example=INPUT_EXAMPLE, 
         data_type="text",
         issue_id=-1,  # you need to look this up in the issues https://github.com/code-kern-ai/bricks/issues
         state=State.DRAFT, # in the actual module, set this to PUBLIC before pushing to main!
