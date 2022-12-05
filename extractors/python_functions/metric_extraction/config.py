0ee13590116beec19f7fe3211a465b7fb02055dd
Leonard Püttmann <leopuettmann@gmail.com>
2022-12-05 16:17:20 +0100

    Renamed function from metric detection to metric extraction

--- extractors/python_functions/metric_extraction/config.py
+++ extractors/python_functions/metric_extraction/config.py
@@ -1,11 +1,11 @@
 from util.configs import build_extractor_function_config
 from util.enums import State
-from . import metric_detection, INPUT_EXAMPLE
+from . import metric_extraction, INPUT_EXAMPLE
 
 
 def get_config():
     return build_extractor_function_config(
-        function=metric_detection,
+        function=metric_extraction,
         input_example=INPUT_EXAMPLE,
         data_type="text",
         issue_id=52,
