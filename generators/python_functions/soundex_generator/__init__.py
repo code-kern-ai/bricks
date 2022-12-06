7e5b3409c35945e4eda8dd667b1921cd6baaefea
Leonard Püttmann <leopuettmann@gmail.com>
2022-12-06 12:28:18 +0100

    SoundEx can now be used on multiple words/ sentences

--- generators/python_functions/soundex_generator/__init__.py
+++ generators/python_functions/soundex_generator/__init__.py
@@ -15,43 +15,48 @@ class SoundexGeneratorModel(BaseModel):
 def soundex_generator(req: SoundexGeneratorModel):
     """Converts an english word into a phonetic SoundEx representation, for example to store names."""
 
-    word = req.word
-    word = unicodedata.normalize("NFKD", word)
-    word = word.upper()
+    input_ = req.word
 
-    replacements = (
-        ("BFPV", "1"),
-        ("CGJKQSXZ", "2"),
-        ("DT", "3"),
-        ("L", "4"),
-        ("MN", "5"),
-        ("R", "6"),
-    )
-    result = [word[0]]
-    count = 1
+    soundex_list = []
+    for word in input_.split():
+        word = unicodedata.normalize("NFKD", word)
+        word = word.upper()
 
-    # find would-be replacment for first character
-    for lset, sub in replacements:
-        if word[0] in lset:
-            last = sub
-            break
-    else:
-        last = None
+        replacements = (
+            ("BFPV", "1"),
+            ("CGJKQSXZ", "2"),
+            ("DT", "3"),
+            ("L", "4"),
+            ("MN", "5"),
+            ("R", "6"),
+        )
+        result = [word[0]]
+        count = 1
 
-    for letter in word[1:]:
+        # find would-be replacment for first character
         for lset, sub in replacements:
-            if letter in lset:
-                if sub != last:
-                    result.append(sub)
-                    count += 1
+            if word[0] in lset:
                 last = sub
                 break
         else:
-            if letter != "H" and letter != "W":
-                # leave last alone if middle letter is H or W
-                last = None
-        if count == 4:
-            break
+            last = None
 
-    result += "0" * (4 - count)
-    return {"SoundEx": "".join(result)}
+        for letter in word[1:]:
+            for lset, sub in replacements:
+                if letter in lset:
+                    if sub != last:
+                        result.append(sub)
+                        count += 1
+                    last = sub
+                    break
+            else:
+                if letter != "H" and letter != "W":
+                    # leave last alone if middle letter is H or W
+                    last = None
+            if count == 4:
+                break
+
+        result += "0" * (4 - count)
+        soundex_list.append("".join(result))
+    
+    return {"SoundEx": " ".join(soundex_list)}
