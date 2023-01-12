b754414b2e7130a2b5f08b5af5e2c4d0801b1e07
Leonard Püttmann <leopuettmann@gmail.com>
2023-01-12 14:16:03 +0100

    Added NYT news search module

---
+++ generators/premiums/nyt_news_search/config.py
@@ -0,0 +1,13 @@
+from util.configs import build_generator_premium_config
+from util.enums import State
+from . import nyt_news_search, INPUT_EXAMPLE
+
+
+def get_config():
+    return build_generator_premium_config(
+        function=nyt_news_search,
+        input_example=INPUT_EXAMPLE,
+        data_type="text",
+        issue_id=209,  
+        state=State.PUBLIC
+    )
