7e836eb881767ecf5615eb78d76eab449a6835f4
Leonard Püttmann <leopuettmann@gmail.com>
2023-01-12 14:30:17 +0100

    Added module to generator init

--- generators/__init__.py
+++ generators/__init__.py
@@ -17,6 +17,7 @@ from .premiums import (
     ibm_translator,
     gpt3_grammar_correction,
     gpt3_tldr_summarization,
+    nyt_news_search,
 )
 
 router = APIRouter()
@@ -37,6 +38,7 @@ for module in [
     text_summarisation,
     gpt3_grammar_correction,
     gpt3_tldr_summarization,
+    nyt_news_search,
 ]:
     module_name = module.__name__.split(".")[-1]
     model_name = (
