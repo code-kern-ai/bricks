d847b6a8972a1c403e0aaaea52c8c61c6dc4d989
Leonard Püttmann <leopuettmann@gmail.com>
2023-02-06 13:55:55 +0100

    Renamed faulty func name in pos extraction

--- extractors/python_functions/part_of_speech_extraction/config.py
+++ extractors/python_functions/part_of_speech_extraction/config.py
@@ -1,11 +1,11 @@
 from util.configs import build_extractor_function_config
 from util.enums import State
-from . import part_of_speech_extractor, INPUT_EXAMPLE
+from . import part_of_speech_extraction, INPUT_EXAMPLE
 
 
 def get_config():
     return build_extractor_function_config(
-        function=part_of_speech_extractor,
+        function=part_of_speech_extraction,
         input_example=INPUT_EXAMPLE,
         data_type="text",
         issue_id=53,
