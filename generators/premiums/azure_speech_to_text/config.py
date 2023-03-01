bfb9769dbb047cfe1ac87396de55a46b8e517501
divyanshu404 <126087614+divyanshu404@users.noreply.github.com>
2023-03-01 16:36:47 +0100

    `BrandWindows` -> `Speakerphone`

--- generators/premiums/azure_speech_to_text/config.py
+++ generators/premiums/azure_speech_to_text/config.py
@@ -9,7 +9,7 @@ def get_config():
         input_example=INPUT_EXAMPLE,
         data_type="text",
         issue_id=250,
-        tabler_icon="BrandWindows",
+        tabler_icon="Speakerphone",
         min_refinery_version="1.7.0",
         state=State.PUBLIC
     )
