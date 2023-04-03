c88c3c7290c1d1d72911cd0ae373046ecc02bf49
d_auras <auras@students.uni-marburg.de>
2023-04-03 18:08:24 +0200

    first working version

---
+++ extractors/python_functions/percentage_extraction/config.py
@@ -0,0 +1,15 @@
+from util.configs import build_extractor_function_config
+from util.enums import State
+from . import percentage_extraction, INPUT_EXAMPLE
+
+
+def get_config():
+    return build_extractor_function_config(
+        function=percentage_extraction,
+        input_example=INPUT_EXAMPLE,
+        data_type="text",
+        issue_id=-1,  # you need to look this up in the issues https://github.com/code-kern-ai/bricks/issues
+        tabler_icon="Template", # Add any fitting icon from tabler-icons.io
+        min_refinery_version="x.x.x",  # you need to look this up in the issues
+        state=State.PUBLIC, # in the actual module, set this to PUBLIC before pushing to main!
+    )
