0620419023372976a5007d57f496b016e2e2e068
d_auras <auras@students.uni-marburg.de>
2023-04-05 17:32:35 +0200

    fixed config.py

--- generators/python_functions/html_unescape/config.py
+++ generators/python_functions/html_unescape/config.py
@@ -1,11 +1,11 @@
 from util.configs import build_generator_function_config
 from util.enums import State
-from . import _template, INPUT_EXAMPLE
+from . import html_unescape, INPUT_EXAMPLE
 
 
 def get_config():
     return build_generator_function_config(
-        function=_template,
+        function=html_unescape,
         input_example=INPUT_EXAMPLE, 
         data_type="text",
         issue_id=233,
