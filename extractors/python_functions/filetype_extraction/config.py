cf04c113f3db60cb944d9ea51b84b8fc714c8423
Ahmed Badr <106032263+Ahmed-Badr01@users.noreply.github.com>
2023-04-10 22:45:59 +0200

    Update config.py

--- extractors/python_functions/filetype_extraction/config.py
+++ extractors/python_functions/filetype_extraction/config.py
@@ -8,7 +8,7 @@ def get_config():
         function=filetype_extraction,
         input_example=INPUT_EXAMPLE,
         data_type="text",
-        issue_id=...,
+        issue_id=272,
         tabler_icon="FileInfo",
         min_refinery_version="1.7.0",
         state=State.PUBLIC,
