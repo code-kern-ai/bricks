691dd53d157c792d4eff3dd13c5cfe022302ea1d
JWittmeyer <jens.wittmeyer@kern.ai>
2023-04-12 11:09:01 +0200

    enum fix

--- util/enums.py
+++ util/enums.py
@@ -4,7 +4,8 @@ from enum import Enum
 class State(Enum):
     DRAFT = "draft"
     PUBLIC = "public"
-    
+
+
 class SelectionType(Enum):
     STRING = "string"
     CHOICE = "choice"
@@ -13,19 +14,21 @@ class SelectionType(Enum):
     FLOAT = "float"
     BOOLEAN = "boolean"
 
+
 class BricksVariableType(Enum):
-    ATTRIBUTE = "attribute",
-    LANGUAGE = "language",
-    LABELING_TASK = "labeling_task",
-    LABEL = "label",
-    EMBEDDING = "embedding",
-    LOOKUP_LIST = "lookup_list",
-    REGEX = "regex",
-    GENERIC_STRING = "generic_string",
-    GENERIC_INT = "generic_int",
-    GENERIC_FLOAT = "generic_float",
-    GENERIC_BOOLEAN = "generic_boolean",
-    UNKNOWN = "unknown",
+    ATTRIBUTE = "attribute"
+    LANGUAGE = "language"
+    LABELING_TASK = "labeling_task"
+    LABEL = "label"
+    EMBEDDING = "embedding"
+    LOOKUP_LIST = "lookup_list"
+    REGEX = "regex"
+    GENERIC_STRING = "generic_string"
+    GENERIC_INT = "generic_int"
+    GENERIC_FLOAT = "generic_float"
+    GENERIC_BOOLEAN = "generic_boolean"
+    UNKNOWN = "unknown"
+
 
 class RefineryDataType(Enum):
     CATEGORY = "category"
