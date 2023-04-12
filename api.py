634862afde0769f6a6763b412c50172be724ba31
JWittmeyer <jens.wittmeyer@kern.ai>
2023-04-12 10:18:59 +0200

    Adds 4455 localhost cors

--- api.py
+++ api.py
@@ -13,6 +13,7 @@ origins = [
     "http://127.0.0.1:3000",
     "http://127.0.0.1:3001",
     "http://localhost",
+    "http://localhost:4455",
     "http://localhost:3000",
     "http://localhost:3001",
     "http://localhost:8000",
