972107a413c3d4de226f685d04c7203220c802e4
Leonard <leonard.puettmann@kern.ai>
2023-09-27 12:15:14 +0200

    updated issue ID and icon

--- classifiers/reference_quality/text_length/config.py
+++ classifiers/reference_quality/text_length/config.py
@@ -7,8 +7,8 @@ def get_config():
     return build_classifier_function_config(
         function=text_length,
         input_example=INPUT_EXAMPLE,
-        issue_id=28,
-        tabler_icon="Books",
+        issue_id=348,
+        tabler_icon="RulerMeasure",
         min_refinery_version="1.7.0",
         state=State.PUBLIC.value,
         type="python_function",
@@ -19,7 +19,7 @@ def get_config():
             "reference_quality",
             "text_analysis"
         ],  # first entry should be parent directory
-        # mapping information for cognition
+        # mapping lables for cognition
         cognition_mapping={
             "short": "Needs fix",
             "medium": "Sufficient",
