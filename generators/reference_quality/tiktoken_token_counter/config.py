1dd58e523d073cf7f0edb3b151b8f4bdc41e4b04
Leonard <leonard.puettmann@kern.ai>
2023-09-28 16:43:22 +0200

    Changed faults import statement

--- generators/reference_quality/tiktoken_token_counter/config.py
+++ generators/reference_quality/tiktoken_token_counter/config.py
@@ -1,10 +1,10 @@
-from util.configs import build_classifier_function_config
+from util.configs import build_generator_function_config
 from util.enums import State, BricksVariableType, RefineryDataType, SelectionType
 from . import tiktoken_token_counter, INPUT_EXAMPLE
 
 
 def get_config():
-    return build_classifier_function_config(
+    return build_generator_function_config(
         # strapi information
         function=tiktoken_token_counter,
         input_example=INPUT_EXAMPLE,
