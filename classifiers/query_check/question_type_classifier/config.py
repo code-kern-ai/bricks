8d6c4d1967871af4673cb3bc9590c4eff4f952d0
Leonard <leonard.puettmann@kern.ai>
2023-10-16 18:24:56 +0200

    added outputs to config

--- classifiers/query_check/question_type_classifier/config.py
+++ classifiers/query_check/question_type_classifier/config.py
@@ -20,6 +20,7 @@ def get_config():
         integrator_inputs={
             "name": "question_type_classifier",
             "refineryDataType": RefineryDataType.TEXT.value,
+            "outputs": ["keyword-question", "statement-question", "interrogative-question"],
             "variables": {
                 "ATTRIBUTE": {
                     "selectionType": SelectionType.CHOICE.value,
